7690fbb4b141204a0df85cd156ca89b53d10c30a
Tippon <thetippon@gmail.com>
2025-08-31 22:09:56 +0100

    Updated to new logo and added featured poster to lineup
    Changes logo to more neutral design and added featured poster to lineup page until lineup poster is complete. Move existing lineup to lineup_tmp while waiting for final details

--- js/footer.js
+++ js/footer.js
@@ -8,6 +8,7 @@ const footerContent = `<br>
 <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
 	<a href="https://www.facebook.com/CwmamanMusicFestival/" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>
 	<a href="https://www.instagram.com/cwmaman_music_festival/" target="_blank"><i class="fa-brands fa-instagram fa-2x"></i></a>
+	<a href="https://bsky.app/profile/cwmfest.co.uk" target="_blank"><i class="fa-brands fa-bluesky fa-2x"></i></a>
 	<a href="https://twitter.com/CwmFest" target="_blank"><i class="fa-brands fa-twitter fa-2x"></i></a>
 	<a href="https://www.flickr.com/photos/127740634@N02/" target="_blank"><i class="fa-brands fa-flickr fa-2x"></i></a>
 	<a href="https://www.youtube.com/user/CwmFest" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>
@@ -15,9 +16,9 @@ const footerContent = `<br>
 <br>
 <credits class="w3-container w3-center w3-content">
 	<h5 class="w3-wide">Website and app design by Ryan at GPD Music</h5>
-	<h5 class="w3-wide">Artwork from 2018 to 2024 by Matthew Palmer and Matt Riste</h5>
-	<h5 class="w3-wide">Artwork and programmes by Su at Bizarre Dragonz</h5>
 	<h5 class="w3-wide">Videos from 2022 onwards by <a href="https://www.youtube.com/channel/UC6WOsXwN3l3s8kUbQgT0DeQ" target="_blank">@PaskyWales</a></h5>
+	<h5 class="w3-wide">Artwork from 2018 to 2024 by Matthew Palmer and Matt Riste</h5>
+	<h5 class="w3-wide">Artwork and programmes to 2024 by Su at Bizarre Dragonz</h5>
 	<h5 class="w3-wide">Cwmaman Music Festival <a href="https://www.cwmfest.co.uk/privacy">privacy policy</a></h5>
 	<br>
 </credits>`;
